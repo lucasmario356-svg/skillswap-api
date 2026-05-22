8d9c5904b27fb91126a7e5e41238e82741a5f2bd
MarioLucasMavru <lucasmario356@gmail.com>
2026-05-22 17:28:48 +0300

    brevo

--- utils/mailer.config.js
+++ utils/mailer.config.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
   }
 });
 
