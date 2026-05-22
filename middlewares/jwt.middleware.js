7ece5af8e3514afb1c07125901420cb2b5bd89f8
MarioLucasMavru <lucasmario356@gmail.com>
2026-05-22 16:53:33 +0300

    Fix middleware y conexion backend activa

--- middlewares/jwt.middleware.js
+++ middlewares/jwt.middleware.js
@@ -1,6 +1,6 @@
-const { expressjwt: jwt } = require("express-jwt");
+const { expressjwt } = require("express-jwt");
 
-const isAuthenticated = jwt({
+const isAuthenticated = expressjwt({
   secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
   requestProperty: "payload",
